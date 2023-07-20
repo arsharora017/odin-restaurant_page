3a5243e15cbab85bdce3bb9dbaf5b8d9d7addfbb
Arshdeep Arora <arsharora017@gmail.com>
2023-07-20 12:08:15 -0400

    add media queries to make webpage responsive for small and medium sized devices

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,26 +3,6 @@ import resImage from "../dist/img/res-photo.jpg";
 const createRestautantHomePage = () => {
   const content = document.querySelector("#content");
 
-  // Create and append headline element
-  // const headline = document.createElement("h1");
-  // headline.textContent = "Welcome to our restaurant!";
-  // pageContent.appendChild(headline);
-
-  // Create and append image elementnpm run build
-
-  // const image = new Image();
-  // image.src = resImage;
-  // image.height = "300";
-  // pageContent.appendChild(image);
-
-  //Create and append copy element
-  // const copy = document.createElement("p");
-  // copy.textContent = "We serve the best food in town. Come and taste it.";
-  // pageContent.appendChild(copy);
-  // content.appendChild(pageContent);
-
-  //creating elements
-
   const pageContent = document.createElement("div");
   const outerDiv = document.createElement("div");
   const heading = document.createElement("h1");
